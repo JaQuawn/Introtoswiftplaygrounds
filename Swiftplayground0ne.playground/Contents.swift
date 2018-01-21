626c38933a8d427404235bfbdaca1fe5f2546b38
jaquawn <jd23@hawaii.edu>
2018-01-20 20:34:19 -1000

    Finished intro to playgrounds assignment 2

--- Swiftplayground0ne.playground/Contents.swift
+++ Swiftplayground0ne.playground/Contents.swift
@@ -11,3 +11,17 @@ var mySecondVariable = "and look i've put two strings together"
 var myThirdVariable = myFirstVariable + mySecondVariable
 print (myThirdVariable)
 
+// Assignment 2 intro to true and false
+
+var appState = false
+print ( appState)
+
+//comparing variables using greater than or less than
+
+var numberOne = 5
+var numberTwo = 7
+
+//Create a variable that holds the comparison on the two variables we created
+
+var  compare = numberOne < numberTwo
+
