9738932edfcada25734f866b0487ec2ab1635965
jaquawn <jd23@hawaii.edu>
2018-01-20 19:33:46 -1000

    Finished swift playgrounds one assignment

--- Swiftplayground0ne.playground/Contents.swift
+++ Swiftplayground0ne.playground/Contents.swift
@@ -2,4 +2,12 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var myFirstVariable: String
+myFirstVariable = " I created my first variable"
+
+// Were going to do something with strings
+var mySecondVariable = "and look i've put two strings together"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print (myThirdVariable)
+
